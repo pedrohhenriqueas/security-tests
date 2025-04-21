b5084923beaf62f0b2fd6de9969da4d6c7bf4913
Pedro Henrique <phas050902@gmail.com>
2025-04-20 23:44:52 -0300

    feat: finishing the institutions crud

--- src/main/java/com/example/prol_educa/controller/InstitutionsController.java
+++ src/main/java/com/example/prol_educa/controller/InstitutionsController.java
@@ -1,13 +1,21 @@
 package com.example.prol_educa.controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.prol_educa.entities.Institutions;
 import com.example.prol_educa.models.InstitutionsDto;
 import com.example.prol_educa.service.InstitutionsService;
 
@@ -21,6 +29,29 @@ public class InstitutionsController {
 	@PostMapping("/create")
 	public ResponseEntity<?> create(@RequestBody InstitutionsDto dto) {
 		service.create(dto);
-		return ResponseEntity.ok("Cadastro da instituição realizado com sucesso!");
+		return ResponseEntity.ok("Cadastro da instituicao realizado com sucesso!");
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Institutions>> findAll(){
+		return service.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public Institutions findById(@PathVariable("id") Integer id) throws Exception{
+		return service.findById(id);
+	}	
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") Integer id,
+								@RequestBody InstitutionsDto dto) throws Exception {
+		service.update(id, dto);
+		return ResponseEntity.ok("Instituição atualizada com sucesso");
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Integer id){
+		service.delete(id);
+		return ResponseEntity.ok("Instituição deletada com sucesso");
 	}
 }
--- src/main/java/com/example/prol_educa/service/InstitutionsService.java
+++ src/main/java/com/example/prol_educa/service/InstitutionsService.java
@@ -1,6 +1,10 @@
 package com.example.prol_educa.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.prol_educa.entities.Institutions;
@@ -16,7 +20,6 @@ public class InstitutionsService {
 
 	public void create(InstitutionsDto dto) {
 		Institutions institution = new Institutions();
-		
 		institution.setName(dto.getName());
 		institution.setStreet(dto.getStreet());
 		institution.setNumber(dto.getNumber());
@@ -33,5 +36,44 @@ public class InstitutionsService {
 		institution.setStatus(dto.isStatus()); 
 		
 		repository.save(institution);
+	}
+	
+	public ResponseEntity<List<Institutions>> findAll() {
+		List<Institutions> institutuions = repository.findAll();
+		
+		return ResponseEntity.ok(institutuions);
+	}
+	
+	public Institutions findById(Integer id) throws Exception {
+		Optional<Institutions> institution = repository.findById(id);
+		if(!institution.isPresent()) {
+			throw new Exception("Instituicao nao encontrada");
+		}
+		
+		return institution.get();
+	}
+	
+	public Institutions update(Integer id, InstitutionsDto dto) throws Exception{
+		Institutions institution = findById(id);
+		institution.setName(dto.getName());
+		institution.setStreet(dto.getStreet());
+		institution.setNumber(dto.getNumber());
+		institution.setComplement(dto.getComplement());
+		institution.setNeighborhood(dto.getNeighborhood());
+		institution.setCity(dto.getCity());
+		institution.setState(dto.getState());
+		institution.setCep(dto.getCep());
+		institution.setName_responsible(dto.getName_responsible());
+		institution.setCell_responsible(dto.getCell_responsible());
+		institution.setEmail_responsible(dto.getEmail_responsible());
+		institution.setType(dto.getType());
+		institution.setUrl_image(dto.getUrl_image());
+		institution.setStatus(dto.isStatus()); 
+		
+		return repository.save(institution);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
 	}
 }
